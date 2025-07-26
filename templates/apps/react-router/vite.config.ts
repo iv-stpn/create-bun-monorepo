a977367fc93c03ed9b6f92a04b5f162fe1f7e3fb
Ivan Stepanian <iv.stpn@gmail.com>
2025-07-26 18:06:35 +0200

    fix svg imports in vite apps

--- templates/apps/react-router/vite.config.ts
+++ templates/apps/react-router/vite.config.ts
@@ -1,8 +1,9 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [reactRouter(), tsconfigPaths()],
+	plugins: [reactRouter(), tsconfigPaths(), svgr()],
 	server: { port: 3004 },
 });
--- templates/apps/react-vike/vite.config.ts
+++ templates/apps/react-vike/vite.config.ts
@@ -1,8 +1,9 @@
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	plugins: [vike(), react()],
+	plugins: [vike(), react(), svgr()],
 	server: { port: 3005 },
 });
